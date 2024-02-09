19dd23f6eb719a5cef250a991ac42ac421d26497
Jasnoor Aulakh <jasgaming02singh@gmail.com>
2024-02-09 13:08:30 -0600

    Create class Factorial

---
+++ Factorial.h
@@ -0,0 +1,18 @@
+//
+// Created by Jasnoor Aulakh on 2/9/24.
+//
+
+#ifndef FACTORIALCALCULATOR_FACTORIAL_H
+#define FACTORIALCALCULATOR_FACTORIAL_H
+
+#include <string>
+
+using namespace std;
+
+class Factorial {
+
+
+};
+
+
+#endif //FACTORIALCALCULATOR_FACTORIAL_H
